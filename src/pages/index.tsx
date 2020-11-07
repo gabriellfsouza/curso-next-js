87f5ac5f1acfc415c9cfebae23980844a4a44648
Gabriel Lima <gabriellfsouza@gmail.com>
2020-11-07 08:50:17 -0300

    server side fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,36 @@
-import { useEffect, useState } from 'react';
-import {Title} from '../styles/pages/Home';
+import { GetServerSideProps } from "next";
+import { useEffect, useState } from "react";
+import { Title } from "../styles/pages/Home";
 
 interface IProduct {
   id: string;
   title: string;
-};
+}
 
-export default function Home() {
-  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+interface HomeProps {
+  recommendedProducts: IProduct[]
+}
 
-  useEffect(()=>{
-    (async()=>{
-      const response = await fetch('http://localhost:3333/recommended');
-      const data = await response.json() as IProduct[];
-      setRecommendedProducts(data);
-    })();
-  },[]);
-
+export default function Home({recommendedProducts}:HomeProps) {
+  
   return (
     <div>
       <section>
         <Title>Products</Title>
         <ul>
-          {recommendedProducts.map(recommendedProduct=>(
+          {recommendedProducts.map((recommendedProduct) => (
             <li key={recommendedProduct.id}>{recommendedProduct.title}</li>
           ))}
         </ul>
-      </section>  
+      </section>
     </div>
-  )
+  );
+}
+
+
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async() => {
+  const response = await fetch("http://localhost:3333/recommended");
+  const recommendedProducts = (await response.json()) as IProduct[];
+  return {props: {recommendedProducts}};
 }
