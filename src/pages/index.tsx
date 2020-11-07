f75b8bc021e76d5434ce1f958e7299a456b416ee
Gabriel Lima <gabriellfsouza@gmail.com>
2020-11-07 08:38:35 -0300

    client side fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from 'react';
 import {Title} from '../styles/pages/Home';
+
+interface IProduct {
+  id: string;
+  title: string;
+};
+
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+
+  useEffect(()=>{
+    (async()=>{
+      const response = await fetch('http://localhost:3333/recommended');
+      const data = await response.json() as IProduct[];
+      setRecommendedProducts(data);
+    })();
+  },[]);
+
   return (
-    <div><Title>Hello World</Title></div>
+    <div>
+      <section>
+        <Title>Products</Title>
+        <ul>
+          {recommendedProducts.map(recommendedProduct=>(
+            <li key={recommendedProduct.id}>{recommendedProduct.title}</li>
+          ))}
+        </ul>
+      </section>  
+    </div>
   )
 }
